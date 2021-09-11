a4cd295248f496f5c5e9583c89a281aa5e695803
Sami Eljabali <sami@eljabali.org>
2021-09-11 00:00:51 -0700

    add more java documentation

--- src/main/kotlin/zoneddatetime/extensions/ZonedDateTimeAttributeExtensions.kt
+++ src/main/kotlin/zoneddatetime/extensions/ZonedDateTimeAttributeExtensions.kt
@@ -2,12 +2,32 @@ package zoneddatetime.extensions
 
 import java.time.ZonedDateTime
 
+/**
+ * Works off of ZonedDateTime context.
+ * @return  True if context's year is a leap year.
+ */
 fun ZonedDateTime.isInLeapYear(): Boolean = YearUtil.isLeapYear(this.year)
 
+/**
+ * Works off of ZonedDateTime context.
+ * @return  True if zoned date time's is at very start of day.
+ */
 fun ZonedDateTime.isAtStartOfDay(): Boolean = this.isEqualTime(this.atStartOfDay())
 
+/**
+ * Works off of ZonedDateTime context.
+ * @return  True if zoned date time's is at very last moment of day.
+ */
 fun ZonedDateTime.isAtEndOfDay(): Boolean = this.isEqualTime(this.atEndOfDay())
 
+/**
+ * Works off of ZonedDateTime context.
+ * @return  Month value of ZonedDateTime base 0, with range 0-11, where 0 is January.
+ */
 fun ZonedDateTime.getMonthBaseZero(): Int = this.monthValue - 1
 
+/**
+ * Works off of ZonedDateTime context.
+ * @return  Number of days found in the particular month of ZonedDateTime, with range 28-31.
+ */
 fun ZonedDateTime.getDaysInMonth(): Int = this.month.length(isInLeapYear())
