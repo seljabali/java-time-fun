749a83d1a55b0bcc3dd6207724342bffffca9506
Sami Eljabali <sami@eljabali.org>
2021-09-10 18:21:30 -0700

    add zoned date time test

--- src/test/kotlin/zoneddatetime/ZonedDateTimeComparisonExtensionsTest.kt
+++ src/test/kotlin/zoneddatetime/ZonedDateTimeComparisonExtensionsTest.kt
@@ -2,6 +2,7 @@ package zoneddatetime
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import zoneddatetime.extensions.*
 
@@ -14,11 +15,15 @@ class ZonedDateTimeComparisonExtensionsTest {
         val dateB = ZonedDateTimeUtil.new(2020, 3, 25)
 
         // when
-        val isDateABeforeDayB = dateA.isBefore(dateB) && dateA.compareDay(dateB) == -1
-        val isDateBAfterDayA = dateB.isAfter(dateA) && dateB.compareDay(dateA) == 1
+        val isDateABeforeDayB =
+            dateA.isBeforeDay(dateB) && dateA.isBeforeEqualDay(dateB) && dateA.compareDay(dateB) == -1
+        val isDateBAfterDayA =
+            dateB.isAfterDay(dateA) && dateB.isAfterEqualDay(dateA) && dateB.compareDay(dateA) == 1
 
-        val isDateABeforeDayTimeB = dateA.isBeforeTime(dateB) && dateA.compareTime(dateB) == -1
-        val isDateBAfterDayTimeA = dateB.isAfterTime(dateA) && dateB.compareTime(dateA) == 1
+        val isDateABeforeDayTimeB =
+            dateA.isBeforeTime(dateB) && dateA.isBeforeEqualTime(dateB) && dateA.compareTime(dateB) == -1
+        val isDateBAfterDayTimeA =
+            dateB.isAfterTime(dateA) && dateB.isAfterEqualTime(dateA) && dateB.compareTime(dateA) == 1
 
         val isDateANotEqualDayDateB = !dateA.isEqualDay(dateB) && !dateB.isEqualDay(dateA)
         val isDateANotEqualDayTimeDateB = !dateA.isEqualTime(dateB) && !dateB.isEqualTime(dateA)
@@ -55,6 +60,47 @@ class ZonedDateTimeComparisonExtensionsTest {
         assertTrue(dateAAndBAreInSameMonth)
 
         assertEquals(dateAAndBDayDifference, 5)
+    }
+
+    @Test
+    fun `given date A & B, when A is same day as B but not same time, then should properly have matching day comparisons`() {
+        // given
+        val dateA = ZonedDateTimeUtil.new(2020, 3, 20)
+        val dateB = ZonedDateTimeUtil.new(2020, 3, 20, 12, 12, 0, 0)
+
+        // when
+        val isDateABeforeDayB = dateA.isBeforeDay(dateB) && dateA.compareDay(dateB) == -1
+        val isDateAEqualDayB =
+            dateA.isBeforeEqualDay(dateB) && dateA.isEqualDay(dateB) && dateA.compareDay(dateB) == 0
+        val isDateBAfterDayA = dateB.isAfterDay(dateA) && dateB.compareDay(dateA) == 1
+        val isDateBAfterEqualDayA = dateB.isAfterEqualDay(dateA)
+        val isDateAEqualDayDateB = dateA.isEqualDay(dateB) && dateB.isEqualDay(dateA)
+
+        // then
+        assertFalse(isDateABeforeDayB)
+        assertTrue(isDateAEqualDayB)
+        assertTrue(isDateBAfterEqualDayA)
+        assertFalse(isDateBAfterDayA)
+        assertTrue(isDateAEqualDayDateB)
+    }
+
+    @Test
+    fun `given date A & B, when A is same day as B but not same time, then should properly have matching time comparisons`() {
+        // given
+        val dateA = ZonedDateTimeUtil.new(2020, 3, 20)
+        val dateB = ZonedDateTimeUtil.new(2020, 3, 20, 12, 12, 0, 0)
+
+        // when
+        val isDateABeforeDayTimeB =
+            dateA.isBeforeTime(dateB) && dateA.isBeforeEqualTime(dateB) && dateA.compareTime(dateB) == -1
+        val isDateBAfterDayTimeA =
+            dateB.isAfterTime(dateA) && dateB.isAfterEqualTime(dateA) && dateB.compareTime(dateA) == 1
+        val isDateAEqualTimeDateB = dateA.isEqualTime(dateB) && dateB.isEqualTime(dateA)
+
+        // then
+        assertTrue(isDateABeforeDayTimeB)
+        assertTrue(isDateBAfterDayTimeA)
+        assertFalse(isDateAEqualTimeDateB)
     }
 
     @Test
