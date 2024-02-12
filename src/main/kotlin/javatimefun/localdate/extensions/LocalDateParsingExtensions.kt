8e26b2e188d4133188193b2c06a5bb5f9ca2a0e3
Sami Eljabali <sami@eljabali.org>
2024-02-11 23:55:32 -0700

    Rename parse methods to use to instead of parse

--- src/main/kotlin/javatimefun/localdate/extensions/LocalDateParsingExtensions.kt
+++ src/main/kotlin/javatimefun/localdate/extensions/LocalDateParsingExtensions.kt
@@ -8,11 +8,11 @@ import java.time.format.DateTimeParseException
  * Works off of String representations of date, without time, nor time zone.
  * When a format is present, it'll try parsing using that format alone, & return null if it fails.
  *
- * @param this  String representation of LocalDate.
- * @param format  String representing format that should solely be used when parsing the date.
- * @return  LocalDate? Null means couldn't parse, else parsed LocalDate.
+ * @param this String representation of LocalDate.
+ * @param format String representing format that should solely be used when parsing the date.
+ * @return LocalDate? Null means couldn't parse, else parsed LocalDate.
  */
-fun String.parseLocalDate(format: String? = null): LocalDate? =
+fun String.toLocalDate(format: String? = null): LocalDate? =
     if (format.isNullOrEmpty()) {
         try {
             LocalDate.parse(this)
